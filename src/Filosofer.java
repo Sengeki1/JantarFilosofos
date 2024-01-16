8a930b43c58bae90fd025652c775fda0f5c206f2
Marco Soares <106749775+Sengeki1@users.noreply.github.com>
2024-01-15 23:10:24 -0100

    fixed while loop

--- src/Filosofer.java
+++ src/Filosofer.java
@@ -1,4 +1,6 @@
+import java.util.Random;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
 
 public class Filosofer extends Thread{
     private int id_filosofer;
@@ -13,14 +15,23 @@ public class Filosofer extends Thread{
 
     @Override
     public void run() {
+        Random rand = new Random(); // random integer
         try {
             while(true) {
                 think();
-                leftFork.acquire();
-                rightFork.acquire();
-                eat();
-                leftFork.release();
-                rightFork.release();
+                leftFork.acquire(); // threads acquires the lock
+                boolean gotRightFork = rightFork.tryAcquire(500, TimeUnit.MILLISECONDS); // try to acquire the rightFork lock, returns a boolean
+                if (gotRightFork) { // critical section
+                    eat(); // if the threads has access to the lock it runs this block of code and release its resources
+                    rightFork.release();
+                    leftFork.release();
+                } else {
+                    leftFork.release(); // if the thread couldn't get the lock since another thread already has the rightFork lock
+                    // it releases its leftFork so when a thread release the resource rightFork, the thread that was on queue is able to
+                    // acquire or compete for the lock again
+                    sleep(rand.nextInt(500)); // makes the thread sleep for a period of random time, so it doesn't
+                    // happen a deadlock or starvation when all the threads try to acquire the Fork simultaneously
+                }
             }
         } catch (Exception e) {
             throw new RuntimeException(e);
@@ -28,10 +39,10 @@ public class Filosofer extends Thread{
     }
 
     public void think() {
-        System.out.println("Filosofer " + this.id_filosofer + " think.");
+        System.out.println("Filosofer " + (this.id_filosofer + 1)+ " think.");
     }
 
     public void eat() {
-        System.out.println("Filosofer " + this.id_filosofer + " eats.");
+        System.out.println("Filosofer " + (this.id_filosofer + 1) + " eats.");
     }
 }
